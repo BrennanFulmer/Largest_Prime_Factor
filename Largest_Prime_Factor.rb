d04460a75a12e36850ecd66cfc3427b7d94c65e9
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-08 17:15:34 -0500

    improved the speed of subcheck if statement by making it start from 3 instead of 2 less than number

--- Largest_Prime_Factor.rb
+++ Largest_Prime_Factor.rb
@@ -2,24 +2,26 @@
 Check is set this low as per Viking Code School's recommendation (quoted exactly below).
 "Don't bother for numbers over 100,000 when using the brute force solution or
 you'll need a few days to calculate"
+However both check and number can be increased as long as check is 2 less than
+number, but the speed takes a hit when check exceeds 9 digits.
 =end
 check = 99999
-number = 600851475143
 primefactor = 0
+number = 600851475143
 
 while 2 < check
 # look for divisible integers of number that are odd and stop when a primefactor has been found (I filtered out evens because the only even prime number is two)
   if number % check == 0 && check % 2 != 0 && primefactor == 0
-    subcheck = check
-    while subcheck > 1
-      subcheck -= 2
+    subcheck = 3
+    while subcheck < check
       if check % subcheck == 0
 # if you find an odd integer that check is divisible by, stop checking the current value of check (this works because I've already ruled out one and checks value)
         break
-      elsif subcheck < 4
-# if you've tried dividing every ood integer lower then check and reached three (or less) then you've got yourself a prime
+      elsif subcheck == check - 2
+# if you've tried dividing every ood integer up to the last odd integer before check then you've got yourself a prime
         primefactor = check
       end
+      subcheck += 2
     end
   end
   check -= 2
